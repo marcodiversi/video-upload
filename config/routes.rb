a09e875b5f04a6ad6ee9ca741766f60d9a8d7032
Marco Diversi <marcodiversi@gmail.com>
2016-10-10 08:24:31 +0000

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
+
+  root 'videos#index'
+  resources :videos
+
+  get '/videos/:id/like' => 'videos#like'
+  get '/videos/:id/dislike' => 'videos#dislike'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+
+  mount Sidekiq::Web => '/sidekiq'
 end
